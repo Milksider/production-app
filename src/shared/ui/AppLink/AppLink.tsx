f2299ec0935e354eff19e400c0d6cdf22cf516bc
PashaBorzenkov <p.borzenkov@purpleplane-it.com>
2025-02-10 21:40:18 +0300

    Added custom AppLink component and navbar

---
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -0,0 +1,27 @@
+import cls from './AppLink.module.scss';
+import {Link, LinkProps} from "react-router-dom";
+import {FC} from "react";
+import {classNames} from "shared/lib/classNames/classNames";
+
+export enum AppLinkTheme {
+    PRIMARY = 'primary',
+    SECONDARY = 'secondary'
+}
+
+interface AppLinkProps extends LinkProps {
+    className?: string;
+    theme?: AppLinkTheme;
+}
+
+export const AppLink: FC<AppLinkProps> = (props: AppLinkProps) => {
+    const {to, children, className, theme = AppLinkTheme.PRIMARY,  ...otherProps} = props;
+    return (
+        <Link
+            className={classNames(cls.AppLink, {}, [className, cls[theme]])}
+            to={to}
+            {...otherProps}
+        >
+            {children}
+        </Link>
+    );
+};
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,19 @@
+import cls from './Navbar.module.scss';
+import {classNames} from "shared/lib/classNames/classNames";
+import {AppLink} from "shared/ui";
+import {AppLinkTheme} from "shared/ui/AppLink/AppLink";
+
+interface NavbarProps {
+    className?: string;
+}
+
+export const Navbar = ({className}: NavbarProps) => {
+    return (
+        <div className={classNames(cls.navbar, {}, [className])}>
+            <div className={cls.links}>
+                <AppLink to="/" className={cls.mainLink} theme={AppLinkTheme.SECONDARY}>Home</AppLink>
+                <AppLink to="/about" theme={AppLinkTheme.SECONDARY}>About</AppLink>
+            </div>
+        </div>
+    );
+};
