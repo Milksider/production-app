ff063520855158a4af01c3d5d9d38b03015e797e
PashaBorzenkov <p.borzenkov@purpleplane-it.com>
2025-02-16 19:08:10 +0300

    fixed another one build errors

--- src/widgets/Sidebar/ui/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar.tsx
@@ -2,6 +2,8 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useState } from 'react';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { LanguageSwitcher } from 'widgets/LanguageSwitcher';
+import { Button } from 'shared/ui/Button';
+import { useTranslation } from 'react-i18next';
 import styles from './Sidebar.module.scss';
 
 export type SidebarProps = {
@@ -10,6 +12,7 @@ export type SidebarProps = {
 
 export const Sidebar = ({ className }: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false);
+    const { t } = useTranslation();
 
     const onToggle = () => {
         setCollapsed((prev) => !prev);
@@ -22,12 +25,12 @@ export const Sidebar = ({ className }: SidebarProps) => {
                 [styles.collapsed]: collapsed,
             }, [className])}
         >
-            {/* <Button */}
-            {/*    onClick={onToggle} */}
-            {/*    data-testid="sidebar-toggle" */}
-            {/* > */}
-            {/*     toggle */}
-            {/* </Button> */}
+            <Button
+                onClick={onToggle}
+                data-testid="sidebar-toggle"
+            >
+                {t('Сайдбар')}
+            </Button>
             <div className={styles.switchers}>
                 <ThemeSwitcher />
                 <LanguageSwitcher className={styles.languageSwitcher} />
