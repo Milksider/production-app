c3fc0534ed46327d76c2fcda3c4f144accff3f58
PashaBorzenkov <p.borzenkov@purpleplane-it.com>
2025-02-16 18:39:14 +0300

    fixed pages stories with dark variant for storybook

--- src/pages/AboutPage/ui/AboutPage.stories.tsx
+++ src/pages/AboutPage/ui/AboutPage.stories.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
 import AboutPage from './AboutPage';
 
 export default {
@@ -20,3 +22,4 @@ Light.args = {
 export const Dark = Template.bind({});
 Dark.args = {
 };
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
