526a2489ba21bfabed0b62b8f1cbd84355153ce3
PashaBorzenkov <p.borzenkov@purpleplane-it.com>
2025-04-03 15:59:36 +0300

    deleted some useless code

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,17 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { counterReducer } from 'entities/Counter/model/slice/counterSlice';
 import { userReducer } from 'entities/User';
 import { loginReducer } from 'features/AuthByUsername';
 
 export function createReduxStore<StateSchema>(initialState?: StateSchema) {
     // const rootReducers: ReducersMapObject<StateSchema> = {
-    //     counter: counterReducer,
     //     user: userReducer,
     // };
 
     return configureStore({
         reducer: {
-            counter: counterReducer,
             user: userReducer,
             loginForm: loginReducer,
         },
--- src/entities/Counter/model/selectors/getCounterValue/index.ts
+++
@@ -1,4 +0,0 @@
-import { createSelector } from '@reduxjs/toolkit';
-import { getCounter } from 'entities/Counter/model/selectors/getCounter';
-
-export const getCounterValue = createSelector(getCounter, (state) => state.value);
